a603f4fd85f66a2882f3d5eea80b692637171b93
Daniel Madalitso Phiri <malgamves@gmail.com>
2024-10-14 17:00:52 +0200

    added new format

---
+++ src/similarity-search/openai/load.ts
@@ -0,0 +1,62 @@
+import weaviate, { WeaviateClient } from 'weaviate-client'
+import axios from 'axios';
+require('dotenv').config();
+
+  async function main() {
+
+    const client =  await weaviate.connectToWeaviateCloud(process.env.WEAVIATE_URL || '',
+        {
+          authCredentials: new weaviate.ApiKey(process.env.WEAVIATE_API_KEY || ''),
+          headers: {
+            'X-OpenAI-Api-Key': process.env.OPENAI_API_KEY || '',  // Replace with your inference API key
+          }
+        } 
+      )
+
+      await client.collections.delete('JeopardyQuestion');;
+      if (await client.collections.exists('JeopardyQuestion') == false) {
+        // lets create and import our collection
+        await client.collections.create({
+            name: 'JeopardyQuestion',
+            properties: [
+              {
+                name: 'Category',
+                dataType: 'text' as const,
+                description: 'Category of the question',
+              },
+              {
+                name: 'Question',
+                dataType: 'text' as const,
+                description: 'The question',
+              },
+              {
+                name: 'Answer',
+                dataType: 'text' as const,
+                description: 'The answer',
+              }
+            ],
+            vectorizers: weaviate.configure.vectorizer.text2VecOpenAI(),
+            generative: weaviate.configure.generative.openAI()
+        
+          });
+
+          try {
+            let jeopardyCollection = await client.collections.get('JeopardyQuestion');
+    
+            const url = 'https://raw.githubusercontent.com/weaviate/weaviate-examples/main/jeopardy_small_dataset/jeopardy_tiny.json'
+            const jeopardyQuestions = await axios.get(url);
+          
+            const res = await jeopardyCollection.data.insertMany(jeopardyQuestions.data)
+          
+            console.log('Data Imported');
+        } catch (e) {
+          console.error(e);
+        }
+      }
+
+
+    
+  }
+  
+  void main();
+  
---
+++ src/similarity-search/openai/query.ts
@@ -0,0 +1,27 @@
+import weaviate, { WeaviateClient } from 'weaviate-client'
+import axios from 'axios';
+require('dotenv').config();
+
+async function main() {
+
+    const client = await weaviate.connectToWeaviateCloud(process.env.WEAVIATE_URL || '',
+        {
+            authCredentials: new weaviate.ApiKey(process.env.WEAVIATE_API_KEY || ''),
+            headers: {
+                'X-OpenAI-Api-Key': process.env.OPENAI_API_KEY || '',  // Replace with your inference API key
+            }
+        })
+
+    const jeopardyCollection = client.collections.get('JeopardyQuestion');
+    const results2 = await jeopardyCollection.query.nearText(['question about animals'])
+    console.log("Near Text objects for:", "search", JSON.stringify(results2, null, 2));
+
+    // Generative Search
+    const myCollection = client.collections.get('JeopardyQuestion');
+    const result = await myCollection.generate.nearText("african elephant in savanna", {
+        singlePrompt: "tell me a quick story about this {question} or {answer}",
+    })
+    console.log("Generated Concept:", result);
+}
+
+void main();
